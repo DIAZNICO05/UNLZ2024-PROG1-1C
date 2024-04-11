bca95148202231b869d1b3fb680b9c86eee96ba3
NICOLAS DIAZ <nico.d.agus2123@gmail.com>
2024-04-11 10:59:52 -0300

    ejercicio2

--- CLASE TRABAJO PRACTICO 2/ejercicio2.cpp
+++ CLASE TRABAJO PRACTICO 2/ejercicio2.cpp
@@ -1,1 +1,20 @@
-hola
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declarar variables
+    float centimetros, metros;
+
+    // Pedir al usuario que ingrese la cantidad en centímetros
+    cout << "Ingrese la longitud en centimetros: ";
+    cin >> centimetros;
+
+    // Convertir centímetros a metros (1 metro = 100 centímetros)
+    metros = centimetros / 100;
+
+    // Mostrar el resultado
+    cout << "La longitud en metros es: " << metros << " metros." << endl;
+
+    return 0;
+}

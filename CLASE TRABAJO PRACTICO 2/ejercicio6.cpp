fc9c8252dfa92719fb409a564bd123967c313db4
DIAZNICO05 <164951588+DIAZNICO05@users.noreply.github.com>
2024-04-15 21:35:41 -0300

    ejercicio6.cpp

--- CLASE TRABAJO PRACTICO 2/ejercicio6.cpp
+++ CLASE TRABAJO PRACTICO 2/ejercicio6.cpp
@@ -3,20 +3,15 @@
 using namespace std;
 
 int main() {
-    // Declarar constante para la velocidad del sonido
-    const float velocidad_sonido = 343; // metros por segundo
+    const float velocidad_sonido = 343; 
 
-    // Declarar variable para el tiempo transcurrido en segundos
     int tiempo_transcurrido;
 
-    // Pedir al usuario que ingrese el tiempo transcurrido en segundos
     cout << "Ingrese el tiempo transcurrido entre el relampago y el trueno en segundos: ";
     cin >> tiempo_transcurrido;
 
-    // Calcular la distancia utilizando la fórmula: distancia = velocidad_sonido * tiempo_transcurrido
     float distancia = velocidad_sonido * tiempo_transcurrido;
 
-    // Mostrar la distancia calculada
     cout << "La distancia a la que se encuentra la tormenta es: " << distancia << " metros." << endl;
 
     return 0;

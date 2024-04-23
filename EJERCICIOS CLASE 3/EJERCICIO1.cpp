83ecc5799cca5f17520d37a83ebfdf2b6266700c
DIAZNICO05 <164951588+DIAZNICO05@users.noreply.github.com>
2024-04-22 23:36:51 -0300

    EJERCICIO1.cpp

--- EJERCICIOS CLASE 3/EJERCICIO1.cpp
+++ EJERCICIOS CLASE 3/EJERCICIO1.cpp
@@ -4,15 +4,12 @@ using namespace std;
 int main() {
     int numero1, numero2, numero3;
 
-    // Solicitar al usuario que ingrese tres números enteros
     cout << "Ingrese el primer número entero: ";
     cin >> numero1;
     cout << "Ingrese el segundo número entero: ";
     cin >> numero2;
     cout << "Ingrese el tercer número entero: ";
     cin >> numero3;
-
-    // Determinar el mayor de los tres números usando una estructura if-else
     if (numero1 >= numero2 && numero1 >= numero3) {
         cout << "El mayor de los tres números es: " << numero1 << endl;
     } else if (numero2 >= numero1 && numero2 >= numero3) {
